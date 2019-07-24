2165248e646aff17e1bdd1fb25b081d7d2677e86
Álvaro Oliveira <alvarogab6@gmail.com>
2019-07-24 18:54:54 -0300

    fix database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-load_dotenv('./config.env')
+load_dotenv()
 
 client = MongoClient(os.getenv('MONGO_HOST'),27017,username=os.getenv('MONGO_USER'),password=os.getenv('MONGO_PASS'))
 
